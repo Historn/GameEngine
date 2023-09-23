71e0d601aab091f1563f1f6147e090cef72ecaf7
Historn <arnaujiga@gmail.com>
2023-09-23 11:02:54 +0200

    Added some new headers and cpp files
    Main
    App
    Globals
    Log
    Module
    Renderer
    Window
    p2List, p2DynArray, p2Point
    Timer

---
+++ TestTemplateEngine/Engine/ModuleRenderer3D.cpp
@@ -0,0 +1,8 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleRenderer3D.h"
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+#include <glad/glad.h>
